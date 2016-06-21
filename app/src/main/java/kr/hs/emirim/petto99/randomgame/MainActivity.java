b4bb0b3613db184bcb2f64497bcc13db6908fec4
JiHyeon Kim <petto99@naver.com>
2016-06-21 20:26:16 +0900

    #1 project

---
+++ app/src/main/java/kr/hs/emirim/petto99/randomgame/MainActivity.java
@@ -0,0 +1,41 @@
+package kr.hs.emirim.petto99.randomgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ViewFlipper flip;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        flip = (ViewFlipper) findViewById(R.id.view_flip);
+        flip.setFlipInterval(1000);
+        Button butStart = (Button)findViewById(R.id.but_text_start);
+        butStart.setOnClickListener(this);
+        Button butStop = (Button)findViewById(R.id.but_text_stop);
+        butStop.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.but_text_start:
+                flip.startFlipping();
+                break;
+            case R.id.but_text_stop:
+                flip.stopFlipping();
+                break;
+        }
+    }
+}
